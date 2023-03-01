51233ba858f6b9a77bf9e2c88c3a53f91a2f9a09
kundan <goswamikundan123@gmail.com>
2023-03-01 17:33:28 +0530

    Adding file which displays two string are anagram or not.

---
+++ Anagrams.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgorithmProblems
+{
+    public class Anagrams
+    {
+        public void Display()
+        {
+            Console.WriteLine("Enter First String");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("Enter Second String");
+            string str2 = Console.ReadLine();
+
+            char[] ch1 = str1.ToUpper().ToCharArray();
+            char[] ch2 = str2.ToUpper().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("They are Anagrams!");
+            }
+            else
+            {
+                Console.WriteLine("Not Anagrams!");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace DSandAlgorithmProblems
+﻿using System;
+
+namespace DSandAlgorithmProblems
 {
     class Program
     {
@@ -12,12 +14,15 @@
             //Insertionsort sort = new Insertionsort();
             //sort.Display();
 
-            int[] arr = { 510, 7, 501, 11, 101 };
-            Console.WriteLine("Array before sorting:");
-            Mergesort.PrintArray(arr);
-            Mergesort.MergeSort(arr, 0, arr.Length - 1);
-            Console.WriteLine("\nArray after sorting:");
-            Mergesort.PrintArray(arr);
+            //int[] arr = { 510, 7, 501, 11, 101 };
+            //Console.WriteLine("Array before sorting:");
+            //Mergesort.PrintArray(arr);
+            //Mergesort.MergeSort(arr, 0, arr.Length - 1);
+            //Console.WriteLine("\nArray after sorting:");
+            //Mergesort.PrintArray(arr);
+
+             Anagrams anagm = new Anagrams();
+            anagm.Display();
         }
     }
 }
