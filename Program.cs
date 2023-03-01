6c35e9c793944934453b0397edd915d0d7cf2c96
kundan <goswamikundan123@gmail.com>
2023-03-01 15:30:30 +0530

    Adding file which displays Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSandAlgorithmProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Master Branch of DS-Algo Problems");
+        }
+    }
+}
