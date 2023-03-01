11de6159791a9cef5a22ab38b50add3568279b90
kundan <goswamikundan123@gmail.com>
2023-03-01 17:16:42 +0530

    Adding Merge sort code

---
+++ Mergesort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgorithmProblems
+{
+    public class Mergesort
+    {
+        public static void MergeSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(arr, left, mid);
+                MergeSort(arr, mid + 1, right);
+
+                Merge(arr, left, mid, right);
+            }
+        }
+
+        public static void Merge(int[] arr, int left, int mid, int right)
+        {
+            int[] temp = new int[arr.Length];
+
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+
+            while (i <= mid && j <= right)
+            {
+                if (arr[i] < arr[j])
+                {
+                    temp[k] = arr[i];
+                    i++;
+                }
+                else
+                {
+                    temp[k] = arr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i <= mid)
+            {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+
+            while (j <= right)
+            {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+
+            for (int x = left; x <= right; x++)
+            {
+                arr[x] = temp[x];
+            }
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            foreach (int x in arr)
+            {
+                Console.Write(x + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,15 @@
             //Bubblesort sort = new  Bubblesort();
             //sort.Display();
 
-            Insertionsort sort = new Insertionsort();
-            sort.Display();
+            //Insertionsort sort = new Insertionsort();
+            //sort.Display();
+
+            int[] arr = { 510, 7, 501, 11, 101 };
+            Console.WriteLine("Array before sorting:");
+            Mergesort.PrintArray(arr);
+            Mergesort.MergeSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nArray after sorting:");
+            Mergesort.PrintArray(arr);
         }
     }
 }
